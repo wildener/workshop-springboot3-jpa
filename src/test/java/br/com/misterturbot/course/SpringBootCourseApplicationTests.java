4554ed933e8f776f3ae926c6fb2eda66618322e0
Wildener Rodovalho <wildener@gmail.com>
2022-12-06 07:20:43 -0300

    Project created

---
+++ src/test/java/br/com/misterturbot/course/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.misterturbot.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
