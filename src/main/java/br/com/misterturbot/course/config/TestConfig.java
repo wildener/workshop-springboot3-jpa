b9afb85fa7011f56470ed15fa6c3ebe43472c45b
Wildener Rodovalho <wildener@gmail.com>
2023-01-17 13:49:16 -0300

    Payment, one-to-one association

--- src/main/java/br/com/misterturbot/course/config/TestConfig.java
+++ src/main/java/br/com/misterturbot/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.misterturbot.course.entity.Category;
 import br.com.misterturbot.course.entity.Order;
 import br.com.misterturbot.course.entity.OrderItem;
+import br.com.misterturbot.course.entity.Payment;
 import br.com.misterturbot.course.entity.Product;
 import br.com.misterturbot.course.entity.User;
 import br.com.misterturbot.course.entity.enums.OrderStatus;
@@ -79,5 +80,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
